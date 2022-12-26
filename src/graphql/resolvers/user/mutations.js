f0ff1b7c62317cc50552467ef08e1f30015dbd75
Kevin Duenas <kevinduenas@hotmail.es>
2022-12-26 10:21:51 -0800

    user update

--- src/graphql/resolvers/user/mutations.js
+++ src/graphql/resolvers/user/mutations.js
@@ -7,12 +7,6 @@ const userMutations = {
   updateUserByToken: async (_, { user }, { user: { id }, loaders }) => {
     const userToSet = { ...user };
 
-    // Users can only update their role if an ADMIN does it through updateUser mutation
-    delete userToSet.overallRole;
-
-    if (userToSet.username)
-      userToSet.username = userToSet.username.toLowerCase();
-
     const updatedUser = await User.findOneAndUpdate(
       { _id: id },
       { $set: { ...userToSet } },
