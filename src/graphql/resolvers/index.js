bb857a969234ba05d9ce3a338d97b928bd442709
Kevin Duenas <kevinduenas@hotmail.es>
2022-12-26 23:47:05 -0800

    Signed URL for AWS S3 Bucket

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,10 +1,12 @@
 import { userQueries, userMutations, userFields } from "./user";
 import { configMutations, configQueries } from "./config";
+import { AWSQueries } from "./aws";
 
 const resolvers = {
   Query: {
     ...userQueries,
     ...configQueries,
+    ...AWSQueries,
     serverDate: () => new Date(),
   },
   Mutation: {
