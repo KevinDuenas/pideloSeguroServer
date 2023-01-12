ce795a5f8a03983ee0d87bd5c97739fe96efb627
Kevin Duenas <kevinduenas@hotmail.es>
2023-01-12 17:22:18 -0600

    Trips from driver

--- src/graphql/resolvers/trip/queries.js
+++ src/graphql/resolvers/trip/queries.js
@@ -1,6 +1,31 @@
 import { User, Config, Trip } from "@db/models";
+import { defaultParams } from "@config/constants";
 import resolveTrip from "@graphql/resolvers/trip";
 
-const tripQueries = {};
+const tripQueries = {
+  driverTrips: async (
+    _,
+    { params = defaultParams },
+    { loaders, user: { id } }
+  ) => {
+    const query = {
+      deleted: false,
+      driver: id,
+    };
+    return {
+      results: async () => {
+        const { page, pageSize } = params;
+        const tripsPromise = Trip.find(query)
+          .skip(pageSize * (page - 1))
+          .limit(pageSize);
+        tripsPromise.sort({ createdAt: "desc" });
+        const results = await tripsPromise;
+        return resolveTrip.many(results, loaders);
+      },
+      count: () => Trip.countDocuments(query),
+      params,
+    };
+  },
+};
 
 export default tripQueries;
