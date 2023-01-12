a734ebb03114b31388b9193440893d047b164400
Kevin Duenas <kevinduenas@hotmail.es>
2023-01-11 20:23:41 -0600

    Adding requestReady dynamic field at userbytoken

--- src/graphql/resolvers/user/fields.js
+++ src/graphql/resolvers/user/fields.js
@@ -1,4 +1,4 @@
-import { User, VerificationRequest } from "@db/models";
+import { User, VerificationRequest, Config } from "@db/models";
 
 const userFields = {
   User: {
@@ -13,6 +13,28 @@ const userFields = {
       if (activeRequest[0].status === "APPROVED") return "APPROVED";
       if (activeRequest[0].status === "REVIEW_PENDING") return "PENDING";
       return "MISSING";
+    },
+    requestReady: async (_, __, { loaders, user: { id: userId } }) => {
+      const activeRequest = await VerificationRequest.find({
+        driver: userId,
+        type: "DRIVER_REQUEST",
+      })
+        .sort({ createdAt: -1 })
+        .limit(1);
+
+      if (activeRequest[0]?.status === "APPROVED") return null;
+
+      const { driverDocuments } = await Config.findOne({});
+      const user = await User.findOne({ _id: userId });
+      if (driverDocuments.length !== user.documents.length) return false;
+      if (
+        !user.firstName ||
+        !user.firstLastName ||
+        !user.secondLastName ||
+        !user.email
+      )
+        return false;
+      return true;
     },
   },
 };
