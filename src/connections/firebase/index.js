c25bfbede189197aba51fc39156a257abab8f96b
Kevin Duenas <kevinduenas@hotmail.es>
2023-02-13 21:49:58 -0600

    Removing debugging log

--- src/connections/firebase/index.js
+++ src/connections/firebase/index.js
@@ -1,7 +1,6 @@
 import fb from "firebase-admin";
 import { firebaseAdminConfig, firebaseDatabaseURL } from "@config/environment";
-console.log("one", firebaseAdminConfig);
-console.log("two", firebaseDatabaseURL);
+
 fb.initializeApp({
   credential: fb.credential.cert(firebaseAdminConfig),
   databaseURL: firebaseDatabaseURL,
