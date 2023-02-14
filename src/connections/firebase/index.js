77e615f26ae10b56533e5e9dce564e4296800975
Kevin Duenas <kevinduenas@hotmail.es>
2023-02-13 21:30:00 -0600

    Debugging log for servers

--- src/connections/firebase/index.js
+++ src/connections/firebase/index.js
@@ -1,6 +1,7 @@
 import fb from "firebase-admin";
 import { firebaseAdminConfig, firebaseDatabaseURL } from "@config/environment";
-
+console.log("one", firebaseAdminConfig);
+console.log("two", firebaseDatabaseURL);
 fb.initializeApp({
   credential: fb.credential.cert(firebaseAdminConfig),
   databaseURL: firebaseDatabaseURL,
