369487518863cda366515b184f2f35e2b211d0c3
Kevin Duenas <kevinduenas@hotmail.es>
2023-02-24 17:29:00 -0600

    Remove default payment method mutations and separating logic between adding a payment method and creating a payment

--- src/graphql/resolvers/payment/mutations.js
+++ src/graphql/resolvers/payment/mutations.js
@@ -1,10 +1,49 @@
 import { User, Payment } from "@db/models";
 import stripe from "@connections/stripe";
+import resolvePayment from "@graphql/resolvers/payment";
 
 const paymentMutations = {
-  createStripePayment: async (_, { amount }, { loaders, user: { id } }) => {
+  createStripePayment: async (
+    _,
+    { amountInCents },
+    { loaders, user: { id } }
+  ) => {
     const user = await User.findOne({ _id: id });
+    if (!user) throw new Error("User was not found.");
+    if (!user.stripeCustomerId)
+      throw new Error("User don't have stripe customer id.");
+    if (amountInCents < 2000 || amountInCents > 50000)
+      throw new Error("Deposit limits: Min 20.00  Max 500.00");
+    const paymentMethod = await stripe.paymentMethods.list({
+      customer: user?.stripeCustomerId,
+      type: "card",
+    });
+    const paymentId = paymentMethod?.data[0]?.id;
+    if (!paymentId) throw new Error("Not payment registered.");
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amountInCents,
+      currency: "mxn",
+      payment_method: paymentId,
+      confirm: "true",
+      off_session: true,
+      customer: user.stripeCustomerId,
+    });
 
+    const newPayment = await new Payment({
+      stripePaymentIntentId: paymentIntent.id,
+      amount: Number(amountInCents / 100).toFixed(2),
+      user: id,
+      status: "INITIALIZED",
+      type: "DRIVER_RECHARGE",
+      method: "STRIPE",
+    });
+
+    const savedPayment = await newPayment.save();
+    return resolvePayment.one(savedPayment, loaders);
+  },
+  addStripePaymentMethod: async (_, __, { loaders, user: { id } }) => {
+    const user = await User.findOne({ _id: id });
+    const amount = 1000;
     if (!user) throw new Error("User was not found.");
     if (!user.stripeCustomerId) {
       const stripeCustomer = await stripe.customers.create({
@@ -36,6 +75,20 @@ const paymentMutations = {
 
     await newPayment.save();
     return paymentIntent.client_secret;
+  },
+  removePaymentMethod: async (_, __, { loaders, user: { id } }) => {
+    if (!id) throw new Error("Access token is required");
+    const user = await User.findOne({ _id: id });
+    if (!user) throw new Error("User not found.");
+    const paymentMethod = await stripe.paymentMethods.list({
+      customer: user?.stripeCustomerId,
+      type: "card",
+    });
+    const paymentId = paymentMethod?.data[0]?.id;
+    if (!paymentId) throw new Error("Not payment method founded.");
+    const deletedPayment = await stripe.paymentMethods.detach(paymentId);
+    if (deletedPayment) return true;
+    return false;
   },
 };
 
